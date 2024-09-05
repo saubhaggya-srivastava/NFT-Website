d8fa25cc92e70546a4c85998e19a2590f21856df
saubhaggya-srivastava <saubhaggya321@gmail.com>
2024-09-05 22:47:09 +0530

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// Navbar for mobile
+
+const navbar = document.querySelector("[data-navbar]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+
+navToggler.addEventListener("click", function () {
+  navbar.classList.toggle("active");
+  this.classList.toggle("active");
+});
+
+// Header and back to top button
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+const activeElementOnScroll = function () {
+  if (window.scrollY > 200) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+}
+
+window.addEventListener("scroll", activeElementOnScroll);
+
